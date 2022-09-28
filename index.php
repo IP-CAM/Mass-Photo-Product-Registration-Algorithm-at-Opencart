82eedb461a6e1ecb7c1a8c97bc531f2b95dadc06
jhonebering-git <jhonebering@gmail.com>
2022-09-28 00:58:32 -0300

    Cria classe para redimendionamento e adição de marca dagua a imagem

--- index.php
+++ index.php
@@ -1,67 +1,23 @@
 <?php
-	//phpinfo();
+	declare(strict_types=1);
+	// header("Content-type: text/html; charset=utf-8");
+	header('Content-Type: application/json');
 
+	require "./src/Models/MarcaDagua.php";
 
-	// imagescale('c.jpg', 200,-1, IMG_NEAREST_NEIGHBOUR);
+	$iMage = new MarcaDagua();
 
-	// die;
+	$iMage->setImagemOriginal('a.jpg');
+	$iMage->setImagemMarcaDagua('marca-dagua.png'); //Obrigatório ser um png
 
-	//set the source image (foreground)
-	$imagemOriginal = 'd.jpg';
+	$iMage->criarImagemComMarcaDagua();
 
-	//set the destination image (background)
-	$marcaDagua = 'marca-dagua.png';
+	$iMage->resizeImagem(4);
+	$iMage->salvarNovaImagem("resultado.jpg", 35);
 
-	//get the size of the source image, needed for imagecopy()
-	list($destWidth, $destHeight) = getimagesize($imagemOriginal);
-	list($srcWidth, $srcHeight) = getimagesize($marcaDagua);
+	$iMage->imprimirResultado();
 
-	// var_dump($srcWidth, $srcHeight);die;
-	
-
-	//create a new image from the source image
-	$srcMarcaDagua = imagecreatefrompng($marcaDagua);
-
-	//create a new image from the destination image
-	$destImagemOriginal = imagecreatefromjpeg($imagemOriginal);
-
-
-	// var_dump($destWidth, $destHeight, $srcWidth, $srcHeight);
-	//merge the source and destination images
-	imagecopyresized($destImagemOriginal, $srcMarcaDagua, 0, 0, 0, 0, $destWidth, $destHeight, $srcWidth, $srcHeight);
-	// imagecopyresized($destImagemOriginal, $srcMarcaDagua, 0, 0, 0, 0, $destWidth, $destHeight, 1250, 860);
-
-
-	$proporcao = 4;
-	$finalWidth = $destWidth / $proporcao ;
-	$finalheight = $destHeight / $proporcao ;
-
-
-	$nova_imagem = imagecreatetruecolor( $finalWidth, $finalheight );
-
-	// Diminui o tamanho da foto de forma proporcional
-	imagecopyresampled($nova_imagem, $destImagemOriginal, 0, 0, 0, 0, $finalWidth, $finalheight, $destWidth, $destHeight);
-	//output the merged images to a file
-	/*
-	 * '100' is an optional parameter,
-	 * it represents the quality of the image to be created,
-	 * if not set, the default is about '75'
-	 */
-	imagejpeg($nova_imagem, 'c.jpg', 35);
-
-
-	// Output and free from memory
-	header('Content-Type: image/gif');
-	imagegif($nova_imagem);
-
-	//destroy the source image
-	imagedestroy($srcMarcaDagua);
-
-	//destroy the destination image
-	imagedestroy($destImagemOriginal);
-	imagedestroy($nova_imagem);
-
-
+	$iMage->destroiImagens();
 
 ?>
 
---
+++ src/Models/MarcaDagua.php
@@ -0,0 +1,87 @@
+<?php
+
+class MarcaDagua 
+{
+	private $_imagemOriginal;
+	private $_oringialWidth;
+	private $_oringialHeight;
+	private $_memoriaImagemOriginal;
+
+	private $_imagemMarcaDagua;
+	private $_marcaDaguaWidth;
+	private $_marcaDaguaHeight;
+	private $_memoriaImagemMarcaDagua;
+
+	function __construct()
+	{
+
+	}
+
+	public function setImagemOriginal($imagem) {
+		$this->_imagemOriginal = $imagem;
+		list($this->_oringialWidth, $this->_oringialHeight) = getimagesize($imagem);
+		$this->_memoriaImagemOriginal = imagecreatefromjpeg($imagem);
+	}
+
+	public function getImagemOriginal() {
+		return $this->_imagemOriginal;
+	}
+
+	public function setImagemMarcaDagua($imagem) {
+		$this->_imagemMarcaDagua = $imagem;
+		list($this->_marcaDaguaWidth, $this->_marcaDaguaHeight) = getimagesize($imagem);
+		$this->_memoriaImagemMarcaDagua = imagecreatefrompng($imagem);
+	}
+
+	public function getImagemMarcaDagua() {
+		return $this->_imagemMarcaDagua;
+	}
+
+	public function criarImagemComMarcaDagua() {
+		imagecopyresized(
+			$this->_memoriaImagemOriginal, 
+			$this->_memoriaImagemMarcaDagua, 
+			0, 0, 0, 0, 
+			$this->_oringialWidth, $this->_oringialHeight, 
+			$this->_marcaDaguaWidth, $this->_marcaDaguaHeight
+		);
+
+		return $this->_memoriaImagemOriginal;
+	}
+
+	public function resizeImagem($proporcao) {
+
+		$widthFinal = $this->_oringialWidth / $proporcao;
+		$heightFinal = $this->_oringialHeight / $proporcao;
+
+		$imagem_temp = imagecreatetruecolor($widthFinal, $heightFinal);
+
+		imagecopyresampled(
+			$imagem_temp, 
+			$this->_memoriaImagemOriginal, 
+			0, 0, 0, 0, 
+			$widthFinal, $heightFinal, 
+			$this->_oringialWidth, $this->_oringialHeight
+		);
+
+		$this->_memoriaImagemOriginal = $imagem_temp;
+	}
+
+	public function salvarNovaImagem($nomeImagem, $qualidade) {
+
+		imagejpeg($this->_memoriaImagemOriginal, $nomeImagem, $qualidade);
+	}
+
+	public function imprimirResultado() {
+		header('Content-Type: image/gif');
+		imagegif($this->_memoriaImagemOriginal);
+	}
+
+	public function destroiImagens() {
+		imagedestroy($this->_memoriaImagemOriginal);
+		imagedestroy($this->_memoriaImagemMarcaDagua);
+	}
+
+}
+
+?>
